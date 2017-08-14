beb75e860000ae8ec7ed436e8053bf6780496cd9
shibbs <steve@alpinelaboratories.com>
2017-08-14 09:36:48 -0600

    update the pin scheme to look for rising pins indicating DUT power is now on. added some comments for future generations (me)

--- spark-main.py
+++ spark-main.py
@@ -7,11 +7,14 @@ GOOD = 1
 NOT_DONE = 2
 
 print('Starting up spark-main')
+print('V1.0 Aug 14')
+
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Set up our pulldowns b/c we are looking for the power to turn on to the DUT
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 load_cmd1 = ("sudo openocd -f spark-ocd1.cfg").split()
 load_cmd2 = ("sudo openocd -f spark-ocd2.cfg").split()
@@ -25,7 +28,7 @@ def isFixtureDone(fixtureNum):
 	file = ""
 	rtn = NOT_DONE
 	if fixtureNum == 1:
-		file = debug1	
+		file = debug1
 	elif fixtureNum ==2:
 		file = debug2
 	else:
@@ -44,20 +47,21 @@ def isFixtureDone(fixtureNum):
 #		print("Didn't find success or error, so probably still chugging")
 	return rtn
 while True:
-	pin1 = GPIO.input(18)
-	pin2 = GPIO.input(19)
-	pin3 = GPIO.input(20)
-	if pin1 == False and last1 == True:
+	# Look for the GPIO pin to go high indicating that the DUT is powered and we're ready to rock.
+	pin1 = GPIO.input(18) #A1
+	pin2 = GPIO.input(19) #A2
+	pin3 = GPIO.input(20) #A3
+	if pin1 == True and last1 == False:
 		print('Loading To Fixture 1')
 		openocd1 = subprocess.Popen(load_cmd1, stdout = debug1)
 		time.sleep(.2)
-	if pin2 == False and last2 == True:
-                print('Loading To Fixture 2')
-                subprocess.Popen(load_cmd2, stdout = debug2)
+	if pin2 == True and last2 == False:
+        print('Loading To Fixture 2')
+        subprocess.Popen(load_cmd2, stdout = debug2)
 		time.sleep(.2)
-	if pin3 == False and last3 == True:
-                print('Loading To Fixture 3')
-                subprocess.Popen(load_cmd3, stdout = debug3)
+	if pin3 == True and last3 == False:
+        print('Loading To Fixture 3')
+        subprocess.Popen(load_cmd3, stdout = debug3)
 		time.sleep(.2)
 	last1 = pin1
 	last2 = pin2

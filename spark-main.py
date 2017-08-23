dd85390b2532bd2ba5f87190eeb14b460a206d13
Steve-Pi <steve@alpinelaboratories.com>
2017-08-23 03:26:01 +0000

    it now tries a few times and seems to catch not connected errors well. However it currently blocks b/c i was too cute with recursion so need to change it up so that all fixtures get a shot and one can't just block

--- spark-main.py
+++ spark-main.py
@@ -20,10 +20,9 @@ load_cmd1 = ("sudo openocd -f spark-ocd1.cfg").split()
 load_cmd2 = ("sudo openocd -f spark-ocd4.cfg").split()
 load_cmd3 = ("sudo openocd -f spark-ocd5.cfg").split()
 
-debug1 = open("debug1.log","wb")
-debug2 = open("debug2.log","wb")
-debug3 = open("debug3.log","wb")
-
+global debug1
+global debug2
+global debug3
 # 
 # def ListenForErrors():
 # 	proc = Popen(monitorCommand.split(), stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True)
@@ -39,58 +38,95 @@ debug3 = open("debug3.log","wb")
 # 	print 'Process is not running'
 # 	return False
 
+def programFixture(fixtureNum, numTries):
+	global debug1
+	global debug2
+	global debug3
+	if fixtureNum == 1:
+		print('Loading To Fixture 1')
+		debug1.close()
+		debug1 = open("debug1.log","wb")
+		subprocess.Popen(load_cmd1, stderr = debug1, stdout = debug1)
+	if fixtureNum == 2 : 
+		print('Loading To Fixture 2')
+		debug2.close()
+		debug2 = open("debug2.log","wb")
+		subprocess.Popen(load_cmd2, stderr = debug2, stdout = debug2)
+	if fixtureNum == 3 :
+		print('Loading To Fixture 3')
+		debug3.close()
+		debug3 = open("debug3.log","wb")
+		subprocess.Popen(load_cmd3, stderr = debug3, stdout = debug3)
+
+	time.sleep(4)
+	result = isFixtureDone(fixtureNum)
+	if numTries == 0:
+                print("**************** We've blown our load tring to program this thing :(")
+	elif result == ERROR:
+		numTries = numTries - 1
+		programFixture(fixtureNum , numTries)
+	else :
+		print("***********Done with Fixture!");
+	
 def isFixtureDone(fixtureNum):
-        _file = ""
-        rtn = NOT_DONE
-        if fixtureNum == 1 :
-                _file = "debug1.log"
-        elif fixtureNum ==2:
-                _file = "debug2.log"
-        else:
-                _file = "debug3.log"
-        with open ( _file) as f:
-                for line in f:
-                        print(line, end = " ")
-                        if "Error" in line:
-                                print("got an error in debug file")
-                                rtn= ERROR
-                                break
-                        elif "Verified OK" in line:
-                                print("Process Succeeded!")
-                                rtn =  GOOD
-                                break
-        if rtn == NOT_DONE:
-                print("Didn't find success or error, so probably still chugging")
-        return rtn
+	parse_file = ""
+	file_obj = "";
+	rtn = NOT_DONE
+	global debug1
+	global debug2
+	global debug3
+	if fixtureNum == 1 :
+		parse_file = "debug1.log"
+		file_obj = debug1
+	elif fixtureNum ==2:
+		parse_file = "debug2.log"
+		file_obj = debug2
+	else:
+		parse_file = "debug3.log"
+		file_obj = debug3
+	with open ( parse_file) as f:
+		for line in f:
+			print(line, end = "/")
+			if "Error: init mode failed" in line:
+				print("****got an error in debug file")
+				rtn= ERROR
+				
+			elif "** Programming Started **" in line:
+				print("Programming started!")
+				rtn =  GOOD
+				
+	if rtn == NOT_DONE:
+		print("Didn't find success or error, so probably still chugging")
+	return rtn
+
+def initLogs():
+	global debug1
+	global debug2
+	global debug3
+	debug1 = open("debug1.log","wb")
+	debug2 = open("debug2.log","wb")
+	debug3 = open("debug3.log","wb")
 
 running1 = running2 = running3 = False
 last1 = last2 = last3 = True
+default_num_tries = 4
+initLogs()
+
 while True:
+	
 	# Look for the GPIO pin to go high indicating that the DUT is powered and we're ready to rock.
 	pin1 = GPIO.input(18) #A1
 	pin2 = GPIO.input(19) #A2
 	pin3 = GPIO.input(20) #A3
 	if pin1 == True and last1 == False:
-		print('Loading To Fixture 1')
-		time.sleep(.2)
-		running1= True;
-		openocd1 = subprocess.Popen(load_cmd1, stdout = debug1)
-		time.sleep(1)
-		isFixtureDone(1);
+				time.sleep(1)
+				programFixture(1, default_num_tries)
 	if pin2 == True and last2 == False:
-		print('Loading To Fixture 2')
-		time.sleep(.2)
-		running2= True;
-		subprocess.Popen(load_cmd2, stdout = debug2)
-		time.sleep(1)
-		isFixtureDone(2);
+				time.sleep(1)
+				programFixture(2, default_num_tries)
 	if pin3 == True and last3 == False:
-		print('Loading To Fixture 3')
-		time.sleep(.2)
-		running3= True;
-		subprocess.Popen(load_cmd3,stderr = debug3, stdout = debug3)
-		time.sleep(1)
-		isFixtureDone(3);
+				time.sleep(1)
+				programFixture(3, default_num_tries)
 	last1 = pin1
 	last2 = pin2
 	last3 = pin3
